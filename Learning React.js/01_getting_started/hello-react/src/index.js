1e8bf0b19938e2cecfc59d5f5739e540d56912c3
plasmatech8 <com0004@gmail.com>
2020-06-22 17:24:12 +1000

    Completed 03

--- Learning React.js/01_getting_started/hello-react/src/index.js
+++ Learning React.js/01_getting_started/hello-react/src/index.js
@@ -2,38 +2,79 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-// Component (alternatively can pass in a single 'props' object)
+const LAKELIST = [
+  {id: 1, name: "Echo Lake"},
+  {id: 2, name: "Maud Lake"},
+  {id: 3, name: "Cascade Lake"}
+]
+
+// Hello Component
 function Hello({foo, hoo, num}){
   return (
-    <div>
+    <>
       <p><b>Welcome to react!!!</b></p>
       <p>{foo}-{hoo}-{num}</p>
       <p><em>Isn't this awesome?!</em></p>
-    </div>
+    </>
   )
 }
 
-// Example of information insertion
-let city = {
-  name: "Madrid",
-  country: "Spain"
-};
+// Lakes Component
+function Lakes({lakes}){
+  return (
+    <>
+      <b>Lakes:</b>
+      <ul>
+        {lakes.map(lake => <li key={lake.id}>{lake.name}</li>)}
+      </ul>
+    </>
+  )
+}
+
+// Base Component
+function App({ lakes, season }){
+
+  // Context info
+  let city = {
+    name: "Madrid",
+    country: "Spain"
+  };
+
+  // Conditionals
+  var see;
+  if (season === "summer"){
+    see = <p>See the lakes!</p>
+  }else{
+    see = <p>See the ski resort!</p>
+  }
+
+  // App JSX
+  return (
+    <>
+      <h1 id="heading" className="cool"> Hello World!</h1>
+      <h2>Basic JSX</h2>
+      <ul>
+        <li>Dog</li>
+        <li>Cat</li>
+        <li>Hamster</li>
+      </ul>
+      <p>{city.name} is in {city.country}</p>
+      <h2>Basic Component</h2>
+      <Hello
+        foo="bar"
+        hoo="raa"
+        num={3}
+      />
+      <h2>List of Items</h2>
+      <Lakes lakes={LAKELIST}/>
+      <h2>Conditional Rendering</h2>
+      {see}
+    </>
+  )
+}
 
 // Render the app
 ReactDOM.render(
-  <div>
-    <h1 id="heading" className="cool"> Hello World!</h1>
-    <ul>
-      <li>Dog</li>
-      <li>Cat</li>
-      <li>Hamster</li>
-    </ul>
-    <p>{city.name} is in {city.country}</p>
-    <Hello
-      foo="bar"
-      hoo="raa"
-      num={3}
-    />
-  </div>,
+  <App season="summer"/>,
   document.getElementById('root')
 );
--- Learning React.js/README.md
+++ Learning React.js/README.md
@@ -60,15 +60,38 @@ Note that className must be used instead of class.
 
 ## React Components
 
-### Components
+### Components and Properties
 
 A component is a function which returns UI elements.
 
 A component function can be used in JSX as a Tag `<MyComponent/>` or `<MyComponent></MyComponent>`
 
-### Properties
-
-We can include the `props` parameter into our component function. Or we use paramter unpacking using `{key1, key2, key3}` syntax.
+We can include the `props` parameter into our component function. Or we use paramter destructuring (unpacking) using `{key1, key2, key3}` syntax.
 
 It will pass in properties of the JSX tag into the function.
 
+### Composing Components
+
+It is good to have an `App` component that we can pass into the render function.
+
+### Lists of Elements/Objects
+
+If we pass a list into JSX (i.e. `{myList}`) it will render the contents.
+
+We can render a list of elements as multiple HTML tags using the **map** function: `{lakes.map(lake => <li key={lake.id}>{lake.name}</li>)}`
+
+We need to add a unique `keys` property when multiple of the same elements are dynamically created.
+
+### Conditional Rendering
+
+We can add if statements in our Component functions to change variables before using `{variableOfJSX}`.
+
+We can also use ternary operators `{condition ? <p>True</p> : <p>False</p>}`.
+
+### React Fragments
+
+JSX expressions must have one parent.
+
+To avoid this, we can use `div`, but that causes clutter.
+
+We can use `React.Fragment` tags instead. Or use the shorthand: `<>...</>`
