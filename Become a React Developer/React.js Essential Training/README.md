bf3120282d6bfafb2d4a52bbac23356de57ccbbc
plasmatech8 <com0004@gmail.com>
2020-06-24 16:52:51 +1000

    Completed 03 components example

--- Become a React Developer/React.js Essential Training/README.md
+++ Become a React Developer/React.js Essential Training/README.md
@@ -31,3 +31,28 @@ ReactDOM.render(
   document.getElementById('root')
 );
 ```
+
+## 03. React Components
+
+Instead of function-based components, we can use class-based components.
+
+```js
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Message extends React.Component {
+  render(){
+    return (
+      <div>
+        <h1 style={{color: this.props.color}}>{this.props.msg}</h1>
+        <p>I'll check back in {this.props.minutes} minutes.</p>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <Message color="blue" msg="how are you?" minutes={8}/>,
+  document.getElementById('root')
+);
+```
--- Become a React Developer/React.js Essential Training/react-is-fun/src/index.js
+++ Become a React Developer/React.js Essential Training/react-is-fun/src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+class Message extends React.Component {
+  render(){
+    return (
+      <div>
+        <h1 style={{color: this.props.color}}>{this.props.msg}</h1>
+        <p>I'll check back in {this.props.minutes} minutes.</p>
+      </div>
+    )
+  }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Message color="blue" msg="how are you?" minutes={8}/>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
