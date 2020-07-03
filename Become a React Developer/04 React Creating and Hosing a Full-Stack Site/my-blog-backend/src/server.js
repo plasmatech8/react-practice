a01fa08970fa06337724c3e5e99b28fde2dac27d
plasmatech8 <com0004@gmail.com>
2020-07-03 17:57:21 +1000

    Clean + add URL param route

--- Become a React Developer/04 React Creating and Hosing a Full-Stack Site/my-blog-backend/src/server.js
+++ Become a React Developer/04 React Creating and Hosing a Full-Stack Site/my-blog-backend/src/server.js
@@ -5,11 +5,8 @@ const app = express();
 app.use(bodyParser.json());
 
 app.get('/hello', (req, res) => res.send('Hello!'));
-app.post('/hello', (req, res) => {
-    res.send(`Hello ${req.body.name}!`);
-    console.log(req.body.name);
-    console.log(req.body);
+app.post('/hello', (req, res) => res.send(`Hello ${req.body.name}!`));
+app.get('/hello/:name', (req, res) => res.send(`Hello ${req.params.name}!`));
 
-});
 
 app.listen(8000, () => console.log('Listening on 8000'));
