bab2367b4659d85b1633e4e56750332436908939
plasmatech8 <com0004@gmail.com>
2020-07-04 16:04:02 +1000

    Added build to server

---
+++ Become a React Developer/04 React Creating and Hosing a Full-Stack Site/my-blog-backend/src/build/precache-manifest.5f21d27c1d4f88ca06a598a3ef0b93ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3460589c095b5c4f61aaf50e268ba873",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ba3c7c32a2f05cce717",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "782125352da1d9f5e6d6",
+    "url": "/static/js/2.9fef1150.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9fef1150.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ba3c7c32a2f05cce717",
+    "url": "/static/js/main.82777e46.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
